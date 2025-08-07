35c45c27b99baeb63338c1b34ef1600a7ff31480
Lucas Grangeiro <lucaslg6401@gmail.com>
2025-08-07 11:51:55 -0300

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { desc } from "drizzle-orm";
 import Image from "next/image";
 
 import CategorySelector from "@/components/common/category-selector";
+import Footer from "@/components/common/footer";
 import { Header } from "@/components/common/header";
 import ProductList from "@/components/common/product-list";
 import { db } from "@/db";
+import { productTable } from "@/db/schema";
 
 const Home = async () => {
   const products = await db.query.productTable.findMany({
@@ -12,7 +15,13 @@ const Home = async () => {
     },
   });
 
-  const categories = await db.query.categoryTable.findMany({})
+  const newlyCreatedProduct = await db.query.productTable.findMany({
+    orderBy: [desc(productTable.createdAt)],
+    with: {
+      variants: true,
+    },
+  });
+  const categories = await db.query.categoryTable.findMany({});
 
   return (
     <>
@@ -45,6 +54,8 @@ const Home = async () => {
             className="h-auto w-full"
           />
         </div>
+        <ProductList products={newlyCreatedProduct} title="Novos produtos" />
+        <Footer />
       </div>
     </>
   );
