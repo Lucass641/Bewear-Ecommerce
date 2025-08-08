e8d71ddb9c7311094beb427f4a2c82ed4aea0da1
Lucas Grangeiro <lucaslg6401@gmail.com>
2025-08-08 12:25:31 -0300

    feat: add product page

--- src/components/common/category-selector.tsx
+++ src/components/common/category-selector.tsx
@@ -10,15 +10,18 @@ interface CategorySelectorProps {
 
 const CategorySelector = ({ categories }: CategorySelectorProps) => {
   return (
-    <div className="rounded-3xl bg-[#F4EFFF] p-6">
+    <div className="rounded-3xl bg-[#F4EFFF] p-5">
       <div className="grid grid-cols-2 gap-3">
         {categories.map((category) => (
           <Button
             key={category.id}
             variant="ghost"
-            className="rounded-full bg-white text-xs font-semibold"
+            className="rounded-full bg-white text-xs font-semibold p-1"
+            type="button"
           >
-            <Link href={`/category/${category.slug}`}>{category.name}</Link>
+            <Link className="w-full" href={`/category/${category.slug}`}>
+              {category.name}
+            </Link>
           </Button>
         ))}
       </div>
